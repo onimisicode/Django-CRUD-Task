9522b6ec9de3974e0a2d02592b0d1dfa01559b54
Your Name <you@example.com>
2022-06-25 17:24:07 +0100

    first commit to Django CRUD Task repo

---
+++ I4G007847CT4/blog/views.py
@@ -0,0 +1,39 @@
+from django.urls import reverse_lazy
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.detail import DetailView
+from .models import Post
+
+# Create your views here.
+class PostListView(ListView):
+   model = Post
+   template_name = 'post_list.html'
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = '__all__'
+    success_url  = reverse_lazy('blog:all')
+    template_name = 'post_form.html'
+
+
+
+class PostDetailView(DeleteView):
+    model = Post
+    template_name = 'post_detail.html'
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = '__all__'
+    success_url  = reverse_lazy('blog:all')
+    template_name = 'post_form.html'
+
+class PostDeleteView(DeleteView):
+    model = Post
+    fields = '__all__'
+    success_url  = reverse_lazy('blog:all')
+    template_name = 'post_confirm_delete.html'
+
+
+
+
+
